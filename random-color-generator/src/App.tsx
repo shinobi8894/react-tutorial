3d5f4d5cfeef8031fe508673a6def712f15e62ef
Tadashi Amano <goldstardev2002@gmail.com>
2024-03-01 21:33:56 -0800

    Random Color generator completed

--- random-color-generator/src/App.tsx
+++ random-color-generator/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  /**
+   * This function `changeColors` is responsible for changing the colors of the application's background dynamically.
+   * It works by generating two new random colors and then applying these colors to the application's root CSS variables.
+   */
+  const changeColors = () => {
+    // Generate two new random colors. Each color is created by generating a random number (using `Math.random`)
+    // which is then multiplied by 16777215 (the maximum value for a 24-bit color) and converted to a hexadecimal string.
+    const newColor1 = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const newColor2 = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    // Apply the newly generated colors to the application's root CSS variables (`--color-1` and `--color-2`).
+    // This is done by accessing the `document.documentElement` (the root element of the document) and setting
+    // its style properties for `--color-1` and `--color-2` to the new colors. This effectively updates the application's
+    // color theme in real-time.
+    document.documentElement.style.setProperty('--color-1', newColor1);
+    document.documentElement.style.setProperty('--color-2', newColor2);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={changeColors}>
+      <h2>Click background to generate new color</h2>
     </div>
   );
 }
