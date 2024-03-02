032e9ed310e905a8e2aeb4591d8aec7ef95524ae
Tadashi Amano <goldstardev2002@gmail.com>
2024-03-01 21:35:18 -0800

    updated function

--- random-color-generator/src/App.tsx
+++ random-color-generator/src/App.tsx
@@ -2,24 +2,27 @@ import './App.css';
 
 function App() {
 
-  /**
-   * This function `changeColors` is responsible for changing the colors of the application's background dynamically.
-   * It works by generating two new random colors and then applying these colors to the application's root CSS variables.
-   */
-  const changeColors = () => {
-    // Generate two new random colors. Each color is created by generating a random number (using `Math.random`)
-    // which is then multiplied by 16777215 (the maximum value for a 24-bit color) and converted to a hexadecimal string.
-    const newColor1 = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    const newColor2 = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  // Function to generate a new random color
+  const generateRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-    // Apply the newly generated colors to the application's root CSS variables (`--color-1` and `--color-2`).
-    // This is done by accessing the `document.documentElement` (the root element of the document) and setting
-    // its style properties for `--color-1` and `--color-2` to the new colors. This effectively updates the application's
-    // color theme in real-time.
-    document.documentElement.style.setProperty('--color-1', newColor1);
-    document.documentElement.style.setProperty('--color-2', newColor2);
+  /**
+   * This function `applyColors` is responsible for applying the given colors to the application's root CSS variables.
+   * It updates the application's color theme in real-time by setting the style properties for `--color-1` and `--color-2`.
+   */
+  const applyColors = (color1: string, color2: string) => {
+    document.documentElement.style.setProperty('--color-1', color1);
+    document.documentElement.style.setProperty('--color-2', color2);
   };
 
+  /**
+   * This function `changeColors` is responsible for changing the colors of the application's background dynamically.
+   * It generates two new random colors and then applies these colors using the `applyColors` function.
+   */
+  const changeColors = () => {
+    const newColor1 = generateRandomColor();
+    const newColor2 = generateRandomColor();
+    applyColors(newColor1, newColor2);
+  };
 
   return (
     <div className="App" onClick={changeColors}>
