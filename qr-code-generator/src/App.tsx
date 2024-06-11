69f42a35fea2198da594d0f5572a9e068d27fc8a
Tadashi <shinobi.design416@gmail.com>
2024-06-10 22:30:25 -0700

    QR Code Generator completed

---
+++ qr-code-generator/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [qrCode, setQrCode] = useState('');
+
+  const generateQRCode = () => {
+    const base64QR = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(inputText)}`;
+    setQrCode(base64QR);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>QR Code Generator</h1>
+        <input 
+          type="text" 
+          placeholder="Enter text here" 
+          value={inputText} 
+          onChange={handleInputChange} 
+        />
+        <button onClick={generateQRCode}>Generate QR Code</button>
+        {qrCode && <img src={qrCode} alt="Generated QR Code" />}
+      </header>
+    </div>
+  );
+}
+
+export default App;
