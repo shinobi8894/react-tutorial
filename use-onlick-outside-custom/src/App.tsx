70dcecb9694e3278e0d57d9cc2f0fdca89ac8430
Tadashi <shinobi.design416@gmail.com>
2024-06-20 01:40:20 -0700

    use onclick outside custom completed

--- use-onlick-outside-custom/src/App.tsx
+++ use-onlick-outside-custom/src/App.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import { useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface RefObject<T> {
+  current: T | null;
+}
+
+type Handler = (event: MouseEvent | TouchEvent) => void;
+
+function useOnClickOutside(ref: RefObject<HTMLElement>, handler: Handler): void {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}
+
+function App(): JSX.Element {
+  const ref = useRef<HTMLElement>(null);
+
+  const handleClickOutside = (): void => {
+    alert('You clicked outside of the header!');
+  };
+
+  useOnClickOutside(ref, handleClickOutside);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header ref={ref} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Click outside of container
         </p>
         <a
           className="App-link"
